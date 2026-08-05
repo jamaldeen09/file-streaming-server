f1306bf969966c456cb5bc5fd95f7ec459081d77
macbook <jamaldeen.o@yahoo.com>
2026-08-06 00:33:32 +0100

    feat: add file uploads to disk

---
+++ src/handlers/upload-file.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import crypto from "crypto"
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+// How this works under the hood:
+// Whatever client makes this request automatically has something we call
+// a TCP socket specifically for them. Well it could be called "socket", TCP
+// is just a protocol that prevents the data being sent via the network from being
+// unordered, or arriving to a destination incomplete. 
+
+// This express server also has a socket which is what is binded to the current machine's
+// operating system. Now when an HTTP request is made (which under the hood is a socket sending some data
+// via the internet/network), the network card of this machine receives the network packets and the OS sees this
+// data and notifies libuv that socket #45 (which is basically our express server conceptually) has some incoming data
+
+// Libuv then notifies Node JS (NOTE: Express created an HTTP server for us, and because of that Node JS
+// creates an HTTP object of some sort that has a parser (lhttp) which is another Node JS thingy. Just know this lhttp is responsible
+// for parsing raw binary data into a proper HTTP reqest with information like the method (GET) the endpoint the request was made to (/api/upload))
+export function uploadFile(req, res) {
+    // Create a unique ID for the file
+    const fileId = crypto.randomUUID();
+
+    // Create folder and file path for this specific file
+    const folderPath = path.join(__dirname, "../uploads");
+    const filePath = path.join(folderPath, `file:${fileId}`);
+
+    // Create a new write stream to write data to a file
+    // in chunks
+    const stream = fs.createWriteStream(filePath, {
+        flags: "a",
+        encoding: 'utf8'
+    });
+
+    // Listen for new chunks of data
+    req.on("data", async function (chunk) {
+        stream.write(chunk)
+    });
+
+    // Forcefully end the stream when there are no more
+    // chunks of data left
+    req.on("end", function () {
+        stream.end();
+    })
+
+    // Listen for when the stream finishes
+    // and send a response back to the HTTP client
+    stream.on("finish", function () {
+        return res.status(201).json({
+            success: true,
+            message: "File has been successfully uploaded",
+            data: { fileId }
+        })
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-console.log("Test")
+// Import the express module
+import express from "express"
+
+// Import the cors module to define specifications
+// on which client can make a request to this server
+// and what HTTP methods they can use when requesting 
+// from this server
+import cors from "cors";
+
+// Import dotenv module, which makes it possible
+// to read the contents of a .env file
+import dotenv from "dotenv";
+import { uploadFile } from "./handlers/upload-file.js"; 
+dotenv.config();
+
+// Extract PORT from environment variables (defaults to 3000, if not defined)
+const port = parseInt(process.env.PORT ?? "3000");
+
+// Create a new express app
+const app = express();
+app.post("/test", (req) => {
+    req.on("data", (data) => {
+        console.log("Data chunk:", data)
+    })
+})
+
+// Apply cors to our express app as a global middleware
+// so it runs before the client's request touches any other endpoints
+// or internal middlewares
+app.use(cors({
+    // "*": any client can make a request to this server
+    origin: "*", 
+
+    // GET - for downloading a file
+    // POST - for uploading a file
+    // DELETE - for deleting a file
+    methods: ["GET", "POST", "DELETE"],
+}));
+
+// ------- Handlers ---------------------
+
+// [File Uploads] - POST /api/upload
+app.post("/api/upload", uploadFile);
+
+// Listen to the port
+// What this does under the hood is that, it tells the c++ library used to communicate
+// with the OS called libuv. It tells libuv to bind this node process to the operating system
+// meaning any network packets the network card in the operating system receives, the operating system
+// basically notifies any ports binded to it. 
+app.listen(port, () => console.log(`Server is running on port: ${port}`))
+
